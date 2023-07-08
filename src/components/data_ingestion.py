b1a71954c5c2fb17f8868202b1838879bc197b66
Kishor-khade <kishorkhade5105@gmail.com>
2023-07-09 00:20:32 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts','train.csv')
-    test_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
     raw_data_path:str = os.path.join('artifacts','data.csv')
     
 class DataIngestion:
