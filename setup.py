e89489022acb90e3f4af0f01ff149e20f0642662
Kishor-khade <kishorkhade5105@gmail.com>
2023-07-17 00:46:33 +0530

    Small change in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='commodity-price-pred',
+    name='commoditypricepredict',
     version='0.0.1',
     author='Kishor',
     author_email='kishorkhade5105@gmail.com',
